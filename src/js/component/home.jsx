958b6e128aa5b50b32672dd897284b8b2463123f
LuisL <dev.eng.luisluna@gmail.com>
2023-02-17 22:21:28 +0000

    version con el codigo terminado y comentado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,11 +5,19 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	// btex valor actual y función de actualizar el estado setbtex, inicializa con un valor vacío "".
 			const [btex,setbtex]=useState("")
+
+	//listadetareas y función de actualizar el estado setListadetareas, inicializa con un array vacío [].		
 			const[listadetareas,setListadetareas]=useState([])
+			
+	//e.preventDefault() para evitar que se recargue la página cuando se envía el formulario. Si no se 
+	//pierde el valor escrito en el formulario		
 			const tareas =(e)=>{e.preventDefault();
 			setListadetareas([...listadetareas,e.target[0].value])
 			}
+
+			
 			const borra =(indexItem)=>{
 			setListadetareas((prevState)=>
 			prevState.filter((elemento,indice)=>indice!==indexItem))
