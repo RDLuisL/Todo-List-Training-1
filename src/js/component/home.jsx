9906ff26da5acf3248287813fd267129385cf19c
LuisL <dev.eng.luisluna@gmail.com>
2023-02-17 22:06:51 +0000

    version con el codigo terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+			const [btex,setbtex]=useState("")
+			const[listadetareas,setListadetareas]=useState([])
+			const tareas =(e)=>{e.preventDefault();
+			setListadetareas([...listadetareas,e.target[0].value])
+			}
+			const borra =(indexItem)=>{
+			setListadetareas((prevState)=>
+			prevState.filter((elemento,indice)=>indice!==indexItem))
+		}
 
-export default Home;
+	return (
+			<div className="text-center">
+				<form type="submit"  onSubmit={tareas}>
+				<h1 className="text-center mt-5">Tareas</h1>
+				<input type="text"  placeholder="¿¿ Que tareas te falta ??" />
+				</form>
+				<ul>{listadetareas.map((elemento,indice)=>{
+					return (<li key={indice}>{elemento}<button onClick={()=>borra(indice)}>🗑️</button></li>)
+				})}
+
+				</ul>
+				
+			</div>
+		);
+	};
+
+	export default Home;
